fd04e9ce0c40b38e8073a28ed8f217f857bb76c8
Eli Pavlov <pavlov.eli@gmail.com>
2023-07-10 07:06:40 +0300

    edit

--- vars.tf
+++ vars.tf
@@ -70,4 +70,42 @@ variable dbname {
   default = "accounts"
 }
 
+variable instance_count {
+  default = "1"
+}
 
+variable dbname {
+  default = "accounts"
+}
+
+variable VPC_NAME {
+  default = "vprofile-VPC"
+}
+
+variable VpcCIDR {
+  default = "172.21.0.0/16"
+}
+
+variable PubSub1CIDR {
+  default = "172.21.1.0/24"
+}
+
+variable PubSub2CIDR {
+  default = "172.21.2.0/24"
+}
+
+variable PubSub3CIDR {
+  default = "172.21.3.0/24"
+}
+
+variable PrivSub1CIDR {
+  default = "172.21.4.0/24"
+}
+
+variable PrivSub2CIDR {
+  default = "172.21.5.0/24"
+}
+
+variable PrivSub3CIDR {
+  default = "172.21.6.0/24"
+}
