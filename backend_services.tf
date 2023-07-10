c62845497eb2aefa54a14eac8f458538b5ac044a
Eli Pavlov <pavlov.eli@gmail.com>
2023-07-10 15:55:14 +0300

    edit

--- backend_services.tf
+++ backend_services.tf
@@ -43,12 +43,6 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
 
 resource "aws_mq_broker" "vprofile-rmq" {
   broker_name = "vprofile-rmw"
-
-  configuration {
-    id       = aws_mq_configuration.test.id
-    revision = aws_mq_configuration.test.latest_revision
-  }
-
   engine_type        = "ActiveMQ"
   engine_version     = "5.15.9"
   host_instance_type = "mq.t2.micro"
